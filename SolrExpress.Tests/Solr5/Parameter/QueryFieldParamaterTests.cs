fba115431ba19be69e592de45b2a9f3ac8d6d985
diegobrum <diego.l.brum@gmail.com>
2015-05-28 21:41:26 -0300

    QueryFieldParamater
    * Qf parameter
    * Unit tests

---
+++ SolrExpress.Tests/Solr5/Parameter/QueryFieldParamaterTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using SolrExpress.Solr5.Parameter;
+
+namespace SolrExpress.Tests.Solr5.Parameter
+{
+    [TestClass]
+    public class QueryFieldParamaterTests
+    {
+        /// <summary>
+        /// Where   Using a QueryFieldParamater instance
+        /// When    Invoking the method "Execute"
+        /// What    Create a valid JSON
+        /// </summary>
+        [TestMethod]
+        public void QueryFieldParamater001()
+        {
+            // Arrange
+            var expected = JObject.Parse(@"
+            {
+              params:{
+                qf:""id^10 score~2^20""
+              }
+            }");
+            string actual;
+            var jObject = new JObject();
+            var paramer = new QueryFieldParamater("id^10 score~2^20");
+
+            // Act
+            paramer.Execute(jObject);
+            actual = jObject.ToString();
+
+            // Assert
+            Assert.AreEqual(expected.ToString(), actual);
+        }
+    }
+}
---
+++ SolrExpress/Solr5/Parameter/QueryFieldParamater.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using SolrExpress.Query;
+
+namespace SolrExpress.Solr5.Parameter
+{
+    public sealed class QueryFieldParamater : IQueryParameter
+    {
+        private readonly JProperty _value;
+
+        /// <summary>
+        /// Create a query field parameter
+        /// </summary>
+        /// <param name="query">Query used to make the query field</param>
+        public QueryFieldParamater(string query)
+        {
+            this._value = new JProperty("qf", query);
+        }
+
+        /// <summary>
+        /// True to indicate multiple instance of the parameter, otherwise false
+        /// </summary>
+        public bool AllowMultipleInstances { get { return true; } }
+
+        /// <summary>
+        /// Parameter name
+        /// </summary>
+        public string ParameterName { get { return "params"; } }
+
+        /// <summary>
+        /// Execute the creation of the parameter "query field"
+        /// </summary>
+        /// <param name="jObject">JSON object with parameters to request to SOLR</param>
+        public void Execute(JObject jObject)
+        {
+            var facetObject = (JObject)jObject[this.ParameterName] ?? new JObject();
+
+            facetObject.Add(this._value);
+
+            jObject[this.ParameterName] = facetObject;
+        }
+    }
+}
