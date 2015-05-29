fe4bd5025008f00ac2a0900d640853b3acfca1b6
diegobrum <diego.l.brum@gmail.com>
2015-05-28 22:08:29 -0300

    MinimumShouldMatchParameter
    * mm parameter
    * unit tests

---
+++ SolrExpress.Tests/Solr5/Parameter/MinimumShouldMatchParameterTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using SolrExpress.Solr5.Parameter;
+
+namespace SolrExpress.Tests.Solr5.Parameter
+{
+    [TestClass]
+    public class MinimumShouldMatchParameterTests
+    {
+        /// <summary>
+        /// Where   Using a MinimumShouldMatchParameter instance
+        /// When    Invoking the method "Execute"
+        /// What    Create a valid JSON
+        /// </summary>
+        [TestMethod]
+        public void MinimumShouldMatchParameter001()
+        {
+            // Arrange
+            var expected = JObject.Parse(@"
+            {
+              params:{
+                mm:""75%""
+              }
+            }");
+            string actual;
+            var jObject = new JObject();
+            var paramer = new MinimumShouldMatchParameter("75%");
+
+            // Act
+            paramer.Execute(jObject);
+            actual = jObject.ToString();
+
+            // Assert
+            Assert.AreEqual(expected.ToString(), actual);
+        }
+    }
+}
---
+++ SolrExpress/Solr5/Parameter/MinimumShouldMatchParameter.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using SolrExpress.Query;
+
+namespace SolrExpress.Solr5.Parameter
+{
+    public sealed class MinimumShouldMatchParameter: IQueryParameter
+    {
+        private readonly JProperty _value;
+
+        /// <summary>
+        /// Create a minimun should parameter parameter
+        /// </summary>
+        /// <param name="expression">Expression used to make the mm parameter</param>
+        public MinimumShouldMatchParameter(string expression)
+        {
+            this._value = new JProperty("mm", expression);
+        }
+
+        /// <summary>
+        /// True to indicate multiple instance of the parameter, otherwise false
+        /// </summary>
+        public bool AllowMultipleInstances { get { return true; } }
+
+        /// <summary>
+        /// Parameter name
+        /// </summary>
+        public string ParameterName { get { return "params"; } }
+
+        /// <summary>
+        /// Execute the creation of the parameter "query field"
+        /// </summary>
+        /// <param name="jObject">JSON object with parameters to request to SOLR</param>
+        public void Execute(JObject jObject)
+        {
+            var facetObject = (JObject)jObject[this.ParameterName] ?? new JObject();
+
+            facetObject.Add(this._value);
+
+            jObject[this.ParameterName] = facetObject;
+        }
+    }
+}
