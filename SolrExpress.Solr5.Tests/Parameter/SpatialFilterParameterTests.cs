1bb2a6b157b4ad4c05c44543779811d3ba0c01f5
diegobrum <diego.l.brum@gmail.com>
2015-06-05 18:01:19 -0300

    SpatialFilterParameter
    * SpatialFilterParameter implementation
    * SpatialFilterParameter unit tests

---
+++ SolrExpress.Solr5.Tests/Parameter/SpatialFilterParameterTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using SolrExpress.Core.Enumerator;
+using SolrExpress.Core.Query;
+using SolrExpress.Solr5.Parameter;
+
+namespace SolrExpress.Solr5.Tests.Parameter
+{
+    [TestClass]
+    public class SpatialFilterParameterTests
+    {
+        /// <summary>
+        /// Where   Using a SpatialFilterParameter instance
+        /// When    Invoking the method "Execute" using geofilt function
+        /// What    Create a valid JSON
+        /// </summary>
+        [TestMethod]
+        public void SpatialFilterParameter001()
+        {
+            // Arrange
+            var expected = JObject.Parse(@"
+            {
+              params:{
+                fq:""{!geofilt sfield=Spatial}"",
+                pt:""-1.1,-2.2"",
+                d:""5.5""
+              }
+            }");
+            string actual;
+            var jObject = new JObject();
+            var parameter = new SpatialFilterParameter<TestDocument>(SolrSpatialFunctionType.Geofilt, q => q.Spatial, new GeoCoordinate(-1.1M, -2.2M), 5.5M);
+
+            // Act
+            parameter.Execute(jObject);
+            actual = jObject.ToString();
+
+            // Assert
+            Assert.AreEqual(expected.ToString(), actual);
+        }
+
+        /// <summary>
+        /// Where   Using a SpatialFilterParameter instance
+        /// When    Invoking the method "Execute" using bbox function
+        /// What    Create a valid JSON
+        /// </summary>
+        [TestMethod]
+        public void SpatialFilterParameter002()
+        {
+            // Arrange
+            var expected = JObject.Parse(@"
+            {
+              params:{
+                fq:""{!bbox sfield=Spatial}"",
+                pt:""-1.1,-2.2"",
+                d:""5.5""
+              }
+            }");
+            string actual;
+            var jObject = new JObject();
+            var parameter = new SpatialFilterParameter<TestDocument>(SolrSpatialFunctionType.Bbox, q => q.Spatial, new GeoCoordinate(-1.1M, -2.2M), 5.5M);
+
+            // Act
+            parameter.Execute(jObject);
+            actual = jObject.ToString();
+
+            // Assert
+            Assert.AreEqual(expected.ToString(), actual);
+        }
+    }
+}
---
+++ SolrExpress.Solr5/Parameter/SpatialFilterParameter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Linq.Expressions;
+using Newtonsoft.Json.Linq;
+using SolrExpress.Core.Enumerator;
+using SolrExpress.Core.Helper;
+using SolrExpress.Core.Query;
+
+namespace SolrExpress.Solr5.Parameter
+{
+    public sealed class SpatialFilterParameter<TDocument> : IParameter
+        where TDocument : IDocument
+    {
+        private readonly SolrSpatialFunctionType _functionType;
+        private readonly Expression<Func<TDocument, object>> _expression;
+        private readonly GeoCoordinate _centerPoint;
+        private readonly decimal _distance;
+
+        /// <summary>
+        /// Create a spatial filter parameter
+        /// </summary>
+        /// <param name="functionType">Function used in the spatial filter</param>
+        /// <param name="expression">Expression used to find the property name</param>
+        /// <param name="centerPoint">Center point to spatial filter</param>
+        /// <param name="distance">Distance from the center point</param>
+        public SpatialFilterParameter(SolrSpatialFunctionType functionType, Expression<Func<TDocument, object>> expression, GeoCoordinate centerPoint, decimal distance)
+        {
+            this._functionType = functionType;
+            this._expression = expression;
+            this._centerPoint = centerPoint;
+            this._distance = distance;
+        }
+
+        /// <summary>
+        /// True to indicate multiple instance of the parameter, otherwise false
+        /// </summary>
+        public bool AllowMultipleInstances { get { return false; } }
+
+        /// <summary>
+        /// Execute the creation of the parameter "sort"
+        /// </summary>
+        /// <param name="jObject">JSON object with parameters to request to SOLR</param>
+        public void Execute(JObject jObject)
+        {
+            var fieldName = UtilHelper.GetPropertyNameFromExpression(this._expression);
+
+            var formule = string.Format("{{!{0} sfield={1}}}", this._functionType.ToString().ToLower(), fieldName);
+
+            var jObj = (JObject)jObject["params"] ?? new JObject();
+
+            jObj.Add(new JProperty("fq", formule));
+            jObj.Add(new JProperty("pt", this._centerPoint.ToString()));
+            jObj.Add(new JProperty("d", this._distance.ToString("0.#", CultureInfo.InvariantCulture)));
+
+            jObject["params"] = jObj;
+        }
+    }
+}
