c3c386092d5a1cf2804e627f71a1c8c3fc16abd6
diegobrum <diego.l.brum@gmail.com>
2015-06-12 11:50:38 -0300

    Issue #9

---
+++ SolrExpress.Solr4.Tests/Parameter/FacetRangeParameterTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolrExpress.Core.Enumerator;
+using SolrExpress.Solr4.Parameter;
+
+namespace SolrExpress.Solr4.Tests.Parameter
+{
+    [TestClass]
+    public class FacetRangeParameterTests
+    {
+        /// <summary>
+        /// Where   Using a FacetRangeParameter instance
+        /// When    Invoking the method "Execute" using the default arguments
+        /// What    Create a valid JSON
+        /// </summary>
+        [TestMethod]
+        public void FacetRangeParameter001()
+        {
+            // Arrange
+            var container = new List<string>();
+            var parameter = new FacetRangeParameter<TestDocument>(q => q.Id, "X", "1", "10", "20");
+
+            // Act
+            parameter.Execute(container);
+
+            // Assert
+            Assert.AreEqual(7, container.Count);
+            Assert.AreEqual("facet=true", container[0]);
+            Assert.AreEqual("facet.range={!ex=dt key=X}Id", container[1]);
+            Assert.AreEqual("f.Id.facet.gap=1", container[2]);
+            Assert.AreEqual("f.Id.facet.start=10", container[3]);
+            Assert.AreEqual("f.Id.facet.end=20", container[4]);
+            Assert.AreEqual("f.Id.facet.other=before", container[5]);
+            Assert.AreEqual("f.Id.facet.other=after", container[6]);
+        }
+
+        /// <summary>
+        /// Where   Using a FacetRangeParameter instance
+        /// When    Invoking the method "Execute" using the sort type and direction parameters
+        /// What    Create a valid JSON
+        /// </summary>
+        [TestMethod]
+        public void FacetRangeParameter002()
+        {
+            // Arrange
+            var container = new List<string>();
+            var parameter = new FacetRangeParameter<TestDocument>(q => q.Id, "X", "1", "10", "20", SolrFacetSortType.CountDesc);
+
+            // Act
+            parameter.Execute(container);
+
+            // Assert
+            Assert.AreEqual(8, container.Count);
+            Assert.AreEqual("facet=true", container[0]);
+            Assert.AreEqual("facet.range={!ex=dt key=X}Id", container[1]);
+            Assert.AreEqual("f.Id.facet.gap=1", container[2]);
+            Assert.AreEqual("f.Id.facet.start=10", container[3]);
+            Assert.AreEqual("f.Id.facet.end=20", container[4]);
+            Assert.AreEqual("f.Id.facet.other=before", container[5]);
+            Assert.AreEqual("f.Id.facet.other=after", container[6]);
+            Assert.AreEqual("f.Id.facet.sort=count", container[7]);
+        }
+    }
+}
---
+++ SolrExpress.Solr4/Parameter/FacetRangeParameter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using SolrExpress.Core.Enumerator;
+using SolrExpress.Core.Helper;
+using SolrExpress.Core.Query;
+
+namespace SolrExpress.Solr4.Parameter
+{
+    public sealed class FacetRangeParameter<T> : IParameter<List<string>>
+      where T : IDocument
+    {
+        private readonly Expression<Func<T, object>> _expression;
+        private readonly string _aliasName;
+        private readonly string _gap;
+        private readonly string _start;
+        private readonly string _end;
+        private readonly SolrFacetSortType? _sortType;
+
+        /// <summary>
+        /// Create a facet parameter
+        /// </summary>
+        /// <param name="expression">Expression used to find the property name</param>
+        /// <param name="aliasName">Name of the alias added in the query</param>
+        /// <param name="gap">Size of each range bucket to make the facet</param>
+        /// <param name="start">Lower bound to make the facet</param>
+        /// <param name="end">Upper bound to make the facet</param>
+        /// <param name="sortType">Sort type of the result of the facet</param>
+        public FacetRangeParameter(Expression<Func<T, object>> expression, string aliasName, string gap = null, string start = null, string end = null, SolrFacetSortType? sortType = null)
+        {
+            this._expression = expression;
+            this._aliasName = aliasName;
+            this._gap = gap;
+            this._start = start;
+            this._end = end;
+            this._sortType = sortType;
+        }
+
+        /// <summary>
+        /// True to indicate multiple instances of the parameter, otherwise false
+        /// </summary>
+        public bool AllowMultipleInstances { get { return true; } }
+
+        /// <summary>
+        /// Execute the creation of the parameter "facet.range"
+        /// </summary>
+        /// <param name="container">Container to parameters to request to SOLR</param>
+        public void Execute(List<string> container)
+        {
+            if (!container.Contains("facet=true"))
+            {
+                container.Add("facet=true");
+            }
+
+            var fieldName = UtilHelper.GetPropertyNameFromExpression(this._expression);
+
+            container.Add(string.Format("facet.range={{!ex=dt key={0}}}{1}", this._aliasName, fieldName));
+
+            if (!string.IsNullOrWhiteSpace(this._gap))
+            {
+                container.Add(string.Format("f.{0}.facet.gap={1}", fieldName, this._gap));
+            }
+            if (!string.IsNullOrWhiteSpace(this._start))
+            {
+                container.Add(string.Format("f.{0}.facet.start={1}", fieldName, this._start));
+            }
+            if (!string.IsNullOrWhiteSpace(this._end))
+            {
+                container.Add(string.Format("f.{0}.facet.end={1}", fieldName, this._end));
+            }
+
+            container.Add(string.Format("f.{0}.facet.other=before", fieldName));
+            container.Add(string.Format("f.{0}.facet.other=after", fieldName));
+
+            if (this._sortType.HasValue)
+            {
+                string typeName;
+                string dummy;
+
+                // TODO: In SOLR 4, we can't choise between ascending or descending sort. Make a choise here, throws a exception case the SolrFacetSortType equals *Descending or not throws???
+                UtilHelper.GetSolrFacetSort(this._sortType.Value, out typeName, out dummy);
+
+                container.Add(string.Format("f.{0}.facet.sort={1}", fieldName, typeName));
+            }
+        }
+    }
+}
