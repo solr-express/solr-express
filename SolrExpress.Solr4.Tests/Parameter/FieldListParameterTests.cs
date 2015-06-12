4411afe1ee7329af6c124102f04420e1ad065f58
diegobrum <diego.l.brum@gmail.com>
2015-06-12 08:34:29 -0300

    Issue #8
    Class and unit test

---
+++ SolrExpress.Solr4.Tests/Parameter/FieldListParameterTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolrExpress.Solr4.Parameter;
+using System.Collections.Generic;
+
+namespace SolrExpress.Solr4.Tests.Parameter
+{
+    [TestClass]
+    public class FieldListParameterTests
+    {
+        /// <summary>
+        /// Where   Using a FieldsParameter instance
+        /// When    Invoking the method "Execute" using 2 instances
+        /// What    Create a valid JSON
+        /// </summary>
+        [TestMethod]
+        public void FieldsParameter001()
+        {
+            // Arrange
+            var container = new List<string>();
+            var parameter1 = new FieldListParameter<TestDocument>(q => q.Id);
+            var parameter2 = new FieldListParameter<TestDocument>(q => q.Score);
+
+            // Act
+            parameter1.Execute(container);
+            parameter2.Execute(container);
+
+            // Assert
+            Assert.AreEqual(1, container.Count);
+            Assert.AreEqual("fl=Id,Score", container[0]);
+        }
+    }
+}
---
+++ SolrExpress.Solr4/Parameter/FieldListParameter.cs
@@ -0,0 +1,53 @@
+﻿using SolrExpress.Core.Helper;
+using SolrExpress.Core.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SolrExpress.Solr4.Parameter
+{
+    public sealed class FieldListParameter<T> : IParameter<List<string>>
+        where T : IDocument
+    {
+        private readonly Expression<Func<T, object>> _expression;
+
+        /// <summary>
+        /// Create a fields parameter
+        /// </summary>
+        /// <param name="expression">Expression used to find the property name</param>
+        public FieldListParameter(Expression<Func<T, object>> expression)
+        {
+            this._expression = expression;
+        }
+
+        /// <summary>
+        /// True to indicate multiple instances of the parameter, otherwise false
+        /// </summary>
+        public bool AllowMultipleInstances { get { return true; } }
+
+        /// <summary>
+        /// Execute the creation of the parameter "fl"
+        /// </summary>
+        /// <param name="container">Container to parameters to request to SOLR</param>
+        public void Execute(List<string> container)
+        {
+            var fieldName = UtilHelper.GetPropertyNameFromExpression(this._expression);
+
+            var fieldList = container.FirstOrDefault(q => q.StartsWith("fl="));
+
+            if (!string.IsNullOrWhiteSpace(fieldList))
+            {
+                container.Remove(fieldList);
+
+                fieldList = string.Concat(fieldList, ",", fieldName);
+            }
+            else
+            {
+                fieldList = string.Concat("fl=", fieldName);
+            }
+
+            container.Add(fieldList);
+        }
+    }
+}
