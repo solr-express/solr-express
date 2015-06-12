490562c072f332731440c0c015081a890558762d
diegobrum <diego.l.brum@gmail.com>
2015-06-12 15:05:40 -0300

    Issue #10

---
+++ SolrExpress.Solr4.Tests/Parameter/FacetQueryParameterTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolrExpress.Core.Enumerator;
+using SolrExpress.Core.ParameterValue;
+using SolrExpress.Solr4.Parameter;
+
+namespace SolrExpress.Solr4.Tests.Parameter
+{
+    [TestClass]
+    public class FacetQueryParameterTests
+    {
+        /// <summary>
+        /// Where   Using a FacetQueryParameter instance
+        /// When    Invoking the method "Execute" using the default arguments
+        /// What    Create a valid string
+        /// </summary>
+        [TestMethod]
+        public void FacetQueryParameter001()
+        {
+            // Arrange
+            var container = new List<string>();
+            var parameter = new FacetQueryParameter("X", new FreeValue("avg('Y')"));
+
+            // Act
+            parameter.Execute(container);
+
+            // Assert
+            Assert.AreEqual(2, container.Count);
+            Assert.AreEqual("facet=true", container[0]);
+            Assert.AreEqual("facet.query={!ex=dt key=X}avg('Y')", container[1]);
+        }
+
+        /// <summary>
+        /// Where   Using a FacetQueryParameter instance
+        /// When    Invoking the method "Execute" using the sort type and direction parameters
+        /// What    Create a valid string
+        /// </summary>
+        [TestMethod]
+        public void FacetQueryParameter002()
+        {
+            // Arrange
+            var container = new List<string>();
+            var parameter = new FacetQueryParameter("X", new FreeValue("avg('Y')"), SolrFacetSortType.CountDesc);
+
+            // Act
+            parameter.Execute(container);
+
+            // Assert
+            Assert.AreEqual(3, container.Count);
+            Assert.AreEqual("facet=true", container[0]);
+            Assert.AreEqual("facet.query={!ex=dt key=X}avg('Y')", container[1]);
+            Assert.AreEqual("f.X.facet.sort=count", container[2]);
+        }
+
+    }
+}
---
+++ SolrExpress.Solr4/Parameter/FacetQueryParameter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using SolrExpress.Core.Enumerator;
+using SolrExpress.Core.Helper;
+using SolrExpress.Core.Query;
+
+namespace SolrExpress.Solr4.Parameter
+{
+    public sealed class FacetQueryParameter : IParameter<List<string>>
+    {
+        private readonly string _aliasName;
+        private readonly IQueryParameterValue _query;
+        private readonly SolrFacetSortType? _sortType;
+
+        /// <summary>
+        /// Create a facet parameter
+        /// </summary>
+        /// <param name="aliasName">Name of the alias added in the query</param>
+        /// <param name="query">Query used to make the facet</param>
+        /// <param name="sortType">Sort type of the result of the facet</param>
+        public FacetQueryParameter(string aliasName, IQueryParameterValue query, SolrFacetSortType? sortType = null)
+        {
+            this._aliasName = aliasName;
+            this._query = query;
+            this._sortType = sortType;
+        }
+
+        /// <summary>
+        /// True to indicate multiple instances of the parameter, otherwise false
+        /// </summary>
+        public bool AllowMultipleInstances { get { return true; } }
+
+        /// <summary>
+        /// Execute the creation of the parameter "sort"
+        /// </summary>
+        /// <param name="container">Container to parameters to request to SOLR</param>
+        public void Execute(List<string> container)
+        {
+            if (!container.Contains("facet=true"))
+            {
+                container.Add("facet=true");
+            }
+
+            var query = this._query.Execute();
+
+            container.Add(string.Format("facet.query={{!ex=dt key={0}}}{1}", this._aliasName, query));
+            
+            if (this._sortType.HasValue)
+            {
+                string typeName;
+                string dummy;
+
+                // TODO: In SOLR 4, we can't choise between ascending or descending sort. Make a choise here, throws a exception case the SolrFacetSortType equals *Descending or not throws???
+                UtilHelper.GetSolrFacetSort(this._sortType.Value, out typeName, out dummy);
+
+                container.Add(string.Format("f.{0}.facet.sort={1}", this._aliasName, typeName));
+            }
+        }
+    }
+}
