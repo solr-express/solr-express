51bef72e6684eaca004219bed9ae549ca9c14cec
diegobrum <diego.l.brum@gmail.com>
2015-06-12 11:06:52 -0300

    Issue #7

---
+++ SolrExpress.Solr4.Tests/Parameter/FilterQueryParameterTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolrExpress.Core.ParameterValue;
+using SolrExpress.Solr4.Parameter;
+
+namespace SolrExpress.Solr4.Tests.Parameter
+{
+    [TestClass]
+    public class FilterQueryParameterTests
+    {
+        /// <summary>
+        /// Where   Using a FilterParameter instance
+        /// When    Invoking the method "Execute" using 2 instances
+        /// What    Create a valid JSON
+        /// </summary>
+        [TestMethod]
+        public void FilterParameter001()
+        {
+            // Arrange
+            var container = new List<string>();
+            var parameter1 = new FilterQueryParameter(new SingleValue<TestDocument>(q => q.Id, "X"));
+            var parameter2 = new FilterQueryParameter(new SingleValue<TestDocument>(q => q.Score, "Y"));
+
+            // Act
+            parameter1.Execute(container);
+            parameter2.Execute(container);
+
+            // Assert
+            Assert.AreEqual(2, container.Count);
+            Assert.AreEqual("fq=Id:X", container[0]);
+            Assert.AreEqual("fq=Score:Y", container[1]);
+        }
+    }
+}
---
+++ SolrExpress.Solr4/Parameter/FilterQueryParameter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using SolrExpress.Core.Query;
+
+namespace SolrExpress.Solr4.Parameter
+{
+    public sealed class FilterQueryParameter : IParameter<List<string>>
+    {
+        private readonly IQueryParameterValue _value;
+
+        /// <summary>
+        /// Create a filter parameter
+        /// </summary>
+        /// <param name="value">Parameter value used to create the query</param>
+        public FilterQueryParameter(IQueryParameterValue value)
+        {
+            this._value = value;
+        }
+
+        /// <summary>
+        /// True to indicate multiple instances of the parameter, otherwise false
+        /// </summary>
+        public bool AllowMultipleInstances { get { return true; } }
+
+        /// <summary>
+        /// Execute the creation of the parameter "fq"
+        /// </summary>
+        /// <param name="container">Container to parameters to request to SOLR</param>
+        public void Execute(List<string> container)
+        {
+            container.Add(string.Concat("fq=", this._value.Execute()));
+        }
+    }
+}
