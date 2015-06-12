c988e769b91b38a2d278fca0f18938d0b9717c99
diegobrum <diego.l.brum@gmail.com>
2015-06-12 11:13:18 -0300

    Issue #5

---
+++ SolrExpress.Solr4.Tests/Parameter/QueryParameterTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolrExpress.Core.ParameterValue;
+using SolrExpress.Solr4.Parameter;
+
+namespace SolrExpress.Solr4.Tests.Parameter
+{
+    [TestClass]
+    public class QueryParameterTests
+    {
+        /// <summary>
+        /// Where   Using a QueryParameter instance
+        /// When    Invoking the method "Execute"
+        /// What    Create a valid JSON
+        /// </summary>
+        [TestMethod]
+        public void QueryParameter001()
+        {
+            // Arrange
+            var container = new List<string>();
+            var parameter = new QueryParameter(new SingleValue<TestDocument>(q => q.Id, "ITEM01"));
+
+            // Act
+            parameter.Execute(container);
+
+            // Assert
+            Assert.AreEqual(1, container.Count);
+            Assert.AreEqual("q=Id:ITEM01", container[0]);
+        }
+    }
+}
---
+++ SolrExpress.Solr4/Parameter/QueryParameter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using SolrExpress.Core.Query;
+
+namespace SolrExpress.Solr4.Parameter
+{
+    public sealed class QueryParameter : IParameter<List<string>>
+    {
+        private readonly IQueryParameterValue _value;
+
+        /// <summary>
+        /// Create a query parameter
+        /// </summary>
+        /// <param name="value">Parameter value used to create the query</param>
+        public QueryParameter(IQueryParameterValue value)
+        {
+            this._value = value;
+        }
+
+        /// <summary>
+        /// True to indicate multiple instances of the parameter, otherwise false
+        /// </summary>
+        public bool AllowMultipleInstances { get { return false; } }
+
+        /// <summary>
+        /// Execute the creation of the parameter "limit"
+        /// </summary>
+        /// <param name="container">Container to parameters to request to SOLR</param>
+        public void Execute(List<string> container)
+        {
+            container.Add(string.Concat("q=", this._value.Execute()));
+        }
+    }
+}
