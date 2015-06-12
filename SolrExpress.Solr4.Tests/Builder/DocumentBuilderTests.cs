cd37b07e338270ad044c880498f527656eaf9b15
diegobrum <diego.l.brum@gmail.com>
2015-06-12 18:05:57 -0300

    Issue #12

---
+++ SolrExpress.Solr4.Tests/Builder/DocumentBuilderTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using SolrExpress.Core.Exception;
+using SolrExpress.Solr4.Builder;
+
+namespace SolrExpress.Solr4.Tests.Builder
+{
+    [TestClass]
+    public class DocumentBuilderTests
+    {
+        /// <summary>
+        /// Where   Using a DocumentBuilder instance
+        /// When    Invoking the method "Execute" using a valid JSON
+        /// What    Parse to informed concret classes
+        /// </summary>
+        [TestMethod]
+        public void DocumentBuilder001()
+        {
+            // Arrange
+            var jsonStr = @"
+            {
+              ""response"":{""numFound"":7722,""start"":0,""maxScore"":1.0,""docs"":[
+                  {
+                    ""id"":""ITEM01"",
+                    ""score"":1.5}]
+              }
+            }";
+            var jsonObject = JObject.Parse(jsonStr);
+            var builder = new DocumentBuilder<TestDocument>();
+            List<TestDocument> lst;
+
+            // Act
+            builder.Execute(jsonObject);
+            lst = builder.Data;
+
+            // Assert
+            Assert.AreEqual(1, lst.Count);
+            Assert.AreEqual("ITEM01", lst[0].Id);
+            Assert.AreEqual(1.5M, lst[0].Score);
+        }
+
+        /// <summary>
+        /// Where   Using a DocumentBuilder instance
+        /// When    Invoking the method "Execute" using a invvalid JSON
+        /// What    Throws UnexpectedJsonFormatException
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(UnexpectedJsonFormatException))]
+        public void DocumentBuilder002()
+        {
+            // Arrange
+            var jsonStr = @"
+            {
+              ""responseX"":{""numFound"":7722,""start"":0,""maxScore"":1.0,""docs"":[
+                  {
+                    ""id"":""ITEM01"",
+                    ""score"":1.5}]
+              }
+            }";
+            var jsonObject = JObject.Parse(jsonStr);
+            var builder = new DocumentBuilder<TestDocument>();
+
+            // Act / Assert
+            builder.Execute(jsonObject);
+        }
+    }
+}
---
+++ SolrExpress.Solr4/Builder/DocumentBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using SolrExpress.Core.Exception;
+using SolrExpress.Core.Query;
+
+namespace SolrExpress.Solr4.Builder
+{
+    /// <summary>
+    /// Document data builder
+    /// </summary>
+    /// <typeparam name="TDocument">Type of the document returned in the search</typeparam>
+    public sealed class DocumentBuilder<TDocument> : IResultBuilder
+        where TDocument : IDocument
+    {
+        /// <summary>
+        /// Execute the JSON object parse in the list of informed document
+        /// </summary>
+        /// <param name="jsonObject">JSON object used in the parse</param>
+        public void Execute(JObject jsonObject)
+        {
+            if ((jsonObject["response"] == null) || (jsonObject["response"]["docs"] == null))
+            {
+                throw new UnexpectedJsonFormatException(jsonObject.ToString());
+            }
+
+            this.Data = jsonObject["response"]["docs"].ToObject<List<TDocument>>();
+        }
+
+        /// <summary>
+        /// Documents of the search
+        /// </summary>
+        public List<TDocument> Data { get; private set; }
+    }
+}
