6fdfbccb1eb23b9c94f7ac41eb15b6c9b536743e
diegobrum <diego.l.brum@gmail.com>
2015-06-13 14:44:49 -0300

    Added QueryAll parameter value

---
+++ SolrExpress.Core/ParameterValue/QueryAll.cs
@@ -0,0 +1,21 @@
+﻿using SolrExpress.Core.Query;
+
+namespace SolrExpress.Core.ParameterValue
+{
+    //TODO: Create unit tests
+
+    /// <summary>
+    /// Query all value parameter
+    /// </summary>
+    public sealed class QueryAll : IQueryParameterValue
+    {
+        /// <summary>
+        /// Execute the parameter value generator
+        /// </summary>
+        /// <returns>Result of the value generator</returns>
+        public string Execute()
+        {
+            return "*:*";
+        }
+    }
+}
