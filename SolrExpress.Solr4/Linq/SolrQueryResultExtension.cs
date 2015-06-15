ea6dce9988de4d97f1be87c833304debaefc6531
diegobrum <diego.l.brum@gmail.com>
2015-06-14 22:28:11 -0300

    Issue #16

---
+++ SolrExpress.Solr4/Linq/SolrQueryResultExtension.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using SolrExpress.Core.Query;
+
+namespace SolrExpress.Solr4.Linq
+{
+    /// <summary>
+    /// SOLR query result extensions methods used to create facilities in the use of the classes
+    /// </summary>
+    public static class SolrQueryResultExtension
+    {
+        /// <summary>
+        /// Returns a document list
+        /// </summary>
+        /// <param name="solrQueryResult">The solr query result</param>
+        /// <param name="data">Documents list</param>
+        public static SolrQueryResult Document<TDocument>(this SolrQueryResult solrQueryResult, out List<TDocument> data)
+            where TDocument : IDocument
+        {
+            data = solrQueryResult.Get(new Solr4.Builder.DocumentBuilder<TDocument>()).Data;
+
+            return solrQueryResult;
+        }
+
+        /// <summary>
+        /// Returns a facet field list
+        /// </summary>
+        /// <param name="solrQueryResult">The solr query result</param>
+        /// <param name="data">Facet field list</param>
+        public static SolrQueryResult FacetField(this SolrQueryResult solrQueryResult, out List<FacetKeyValue<string>> data)
+        {
+            data = solrQueryResult.Get(new Solr4.Builder.FacetFieldResultBuilder()).Data;
+
+            return solrQueryResult;
+        }
+
+        /// <summary>
+        /// Returns a facet query list
+        /// </summary>
+        /// <param name="solrQueryResult">The solr query result</param>
+        /// <param name="data">Facet query list</param>
+        public static SolrQueryResult FacetQuery(this SolrQueryResult solrQueryResult, out Dictionary<string, long> data)
+        {
+            data = solrQueryResult.Get(new Solr4.Builder.FacetQueryResultBuilder()).Data;
+
+            return solrQueryResult;
+        }
+
+        /// <summary>
+        /// Returns a facet range list
+        /// </summary>
+        /// <param name="solrQueryResult">The solr query</param>
+        /// <param name="data">Facet range list</param>
+        public static SolrQueryResult FacetRange(this SolrQueryResult solrQueryResult, out List<FacetKeyValue<FacetRange>> data)
+        {
+            data = solrQueryResult.Get(new Solr4.Builder.FacetRangeResultBuilder()).Data;
+
+            return solrQueryResult;
+        }
+
+        /// <summary>
+        /// Returns statistics about the search
+        /// </summary>
+        /// <param name="solrQueryResult">The solr query</param>
+        /// <param name="isEmpty">True if search result return empty result, false otherwise</param>
+        /// <param name="documentCount">Total quantity of documents in the result</param>
+        /// <param name="timeToExecution">Time to SOLR process the requested search</param>
+        public static SolrQueryResult Statistic(this SolrQueryResult solrQueryResult, out bool isEmpty, out long documentCount, out TimeSpan timeToExecution)
+        {
+            var builder = solrQueryResult.Get(new Solr4.Builder.StatisticResultBuilder());
+            isEmpty = builder.IsEmpty;
+            documentCount = builder.DocumentCount;
+            timeToExecution = builder.TimeToExecution;
+
+            return solrQueryResult;
+        }
+    }
+}
---
+++ SolrExpress.Solr4/Linq/SolrQueryableExtension.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Linq.Expressions;
+using SolrExpress.Core.Enumerator;
+using SolrExpress.Core.ParameterValue;
+using SolrExpress.Core.Query;
+using SolrExpress.Solr4.Parameter;
+
+namespace SolrExpress.Solr4.Linq
+{
+    /// <summary>
+    /// SOLR queryable extensions methods used to create facilities in the use of the classes
+    /// </summary>
+    public static class SolrQueryableExtension
+    {
+        /// <summary>
+        /// Create a facet field parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="expression">Expression used to find the property name</param>
+        /// <param name="sortType">Sort type of the result of the facet</param>
+        public static SolrQueryable<TDocument> FacetField<TDocument>(this SolrQueryable<TDocument> solrQueryable, Expression<Func<TDocument, object>> expression, SolrFacetSortType? sortType = null)
+            where TDocument : IDocument
+        {
+            return solrQueryable.Parameter(new FacetFieldParameter<TDocument>(expression, sortType));
+        }
+
+        /// <summary>
+        /// Create a facet query parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="aliasName">Name of the alias added in the query</param>
+        /// <param name="query">Query used to make the facet</param>
+        /// <param name="sortType">Sort type of the result of the facet</param>
+        public static SolrQueryable<TDocument> FacetQuery<TDocument>(this SolrQueryable<TDocument> solrQueryable, string aliasName, IQueryParameterValue query, SolrFacetSortType? sortType = null)
+            where TDocument : IDocument
+        {
+            return solrQueryable.Parameter(new FacetQueryParameter(aliasName, query, sortType));
+        }
+
+        /// <summary>
+        /// Create a facet range parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="expression">Expression used to find the property name</param>
+        /// <param name="aliasName">Name of the alias added in the query</param>
+        /// <param name="gap">Size of each range bucket to make the facet</param>
+        /// <param name="start">Lower bound to make the facet</param>
+        /// <param name="end">Upper bound to make the facet</param>
+        /// <param name="sortType">Sort type of the result of the facet</param>
+        public static SolrQueryable<TDocument> FacetRange<TDocument>(this SolrQueryable<TDocument> solrQueryable, Expression<Func<TDocument, object>> expression, string aliasName, string gap = null, string start = null, string end = null, SolrFacetSortType? sortType = null)
+            where TDocument : IDocument
+        {
+            return solrQueryable.Parameter(new FacetRangeParameter<TDocument>(expression, aliasName, gap, start, end, sortType));
+        }
+
+        /// <summary>
+        /// Create a fields parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="expression">Expression used to find the property name</param>
+        public static SolrQueryable<TDocument> FieldList<TDocument>(this SolrQueryable<TDocument> solrQueryable, Expression<Func<TDocument, object>> expression)
+            where TDocument : IDocument
+        {
+            return solrQueryable.Parameter(new FieldListParameter<TDocument>(expression));
+        }
+
+        /// <summary>
+        /// Create a filter parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="expression">Expression used to find the property name</param>
+        /// <param name="value">Value of the filter</param>
+        public static SolrQueryable<TDocument> FilterQuery<TDocument>(this SolrQueryable<TDocument> solrQueryable, Expression<Func<TDocument, object>> expression, string value)
+            where TDocument : IDocument
+        {
+            var paramaterValue = new SingleValue<TDocument>(expression, value);
+            return solrQueryable.Parameter(new FilterQueryParameter(paramaterValue));
+        }
+
+        /// <summary>
+        /// Create a filter parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="expression">Expression used to find the property name</param>
+        /// <param name="from">From value in a range filter</param>
+        /// <param name="to">To value in a range filter</param>
+        public static SolrQueryable<TDocument> FilterQuery<TDocument, TValue>(this SolrQueryable<TDocument> solrQueryable, Expression<Func<TDocument, object>> expression, TValue? from, TValue? to)
+            where TDocument : IDocument
+            where TValue : struct
+        {
+            var value = new RangeValue<TDocument, TValue>(expression, from, to);
+            return solrQueryable.Parameter(new FilterQueryParameter(value));
+        }
+        
+        /// <summary>
+        /// Create a rows parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="value">Parameter to include in the query</param>
+        public static SolrQueryable<TDocument> Rows<TDocument>(this SolrQueryable<TDocument> solrQueryable, int value)
+            where TDocument : IDocument
+        {
+            return solrQueryable.Parameter(new RowsParameter(value));
+        }
+
+        /// <summary>
+        /// Create a start parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="value">Parameter to include in the query</param>
+        public static SolrQueryable<TDocument> Start<TDocument>(this SolrQueryable<TDocument> solrQueryable, int value)
+            where TDocument : IDocument
+        {
+            return solrQueryable.Parameter(new StartParameter(value));
+        }
+
+        /// <summary>
+        /// Create a query parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="value">Parameter to include in the query</param>
+        public static SolrQueryable<TDocument> Query<TDocument>(this SolrQueryable<TDocument> solrQueryable, string value)
+            where TDocument : IDocument
+        {
+            var freeValue = new FreeValue(value);
+            return solrQueryable.Parameter(new QueryParameter(freeValue));
+        }
+
+        /// <summary>
+        /// Create a query parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="expression">Expression used to find the property name</param>
+        /// <param name="value">Value of the query</param>
+        public static SolrQueryable<TDocument> Query<TDocument>(this SolrQueryable<TDocument> solrQueryable, Expression<Func<TDocument, object>> expression, string value)
+            where TDocument : IDocument
+        {
+            var paramaterValue = new SingleValue<TDocument>(expression, value);
+            return solrQueryable.Parameter(new QueryParameter(paramaterValue));
+        }
+
+        /// <summary>
+        /// Create a sort parameter
+        /// </summary>
+        /// <param name="solrQueryable">The solr query</param>
+        /// <param name="expression">Expression used to find the property name</param>
+        /// <param name="ascendent">True to ascendent order, otherwise false</param>
+        public static SolrQueryable<TDocument> Sort<TDocument>(this SolrQueryable<TDocument> solrQueryable, Expression<Func<TDocument, object>> expression, bool ascendent)
+            where TDocument : IDocument
+        {
+            return solrQueryable.Parameter(new SortParameter<TDocument>(expression, ascendent));
+        }
+    }
+}
