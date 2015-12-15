8590775c255b8dbc237af6e9095921bb46128ce4
diegobrum <diego.l.brum@gmail.com>
2015-12-15 16:55:31 -0200

    #81

--- SolrExpress.Solr4/Properties/AssemblyInfo.cs
+++ SolrExpress.Solr4/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("SolrExpress.Solr4")]
 [assembly: AssemblyDescription("A simple and lightweight query .NET library for Solr 4.x")]
 [assembly: AssemblyConfiguration("")]
@@ -13,27 +10,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6900d200-26dc-400b-a3ac-359cd75b69bb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.1")]
-[assembly: AssemblyFileVersion("1.1.0.1")]
+[assembly: AssemblyVersion("1.1.0.2")]
+[assembly: AssemblyFileVersion("1.1.0.2")]
 
 #if STRONGNAME
 [assembly: AssemblyKeyFile("SolrExpress.Solr4.snk")]
