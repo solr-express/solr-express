48d72c44704aea75730038950e7ff70ebab696f8
Diego <diego.l.brum@gmail.com>
2016-04-18 22:16:07 -0300

    AnyParameter

--- SolrExpress.Solr5/Parameter/AnyParameter.cs
+++ SolrExpress.Solr5/Parameter/AnyParameter.cs
@@ -1,29 +1,30 @@
 ﻿using Newtonsoft.Json.Linq;
 using SolrExpress.Core.Parameter;
-using SolrExpress.Core.Query;
 
 namespace SolrExpress.Solr5.Parameter
 {
     public sealed class AnyParameter : IAnyParameter, IParameter<JObject>
     {
-        private readonly string _name;
-        private readonly string _value;
-
-        /// <summary>
-        /// Create any parameter
-        /// </summary>
-        /// <param name="name">Name of the parameter</param>
-        /// <param name="value">Value of the parameter</param>
-        public AnyParameter(string name, string value)
-        {
-            this._name = name;
-            this._value = value;
-        }
+        private string _name;
+        private string _value;
 
         /// <summary>
         /// True to indicate multiple instances of the parameter, otherwise false
         /// </summary>
         public bool AllowMultipleInstances { get; } = true;
+
+        /// <summary>
+        /// Configure current instance
+        /// </summary>
+        /// <param name="name">Name of the parameter</param>
+        /// <param name="value">Value of the parameter</param>
+        public IAnyParameter Configure(string name, string value)
+        {
+            this._name = name;
+            this._value = value;
+
+            return this;
+        }
 
         /// <summary>
         /// Execute the creation of the parameter
@@ -32,7 +33,7 @@ namespace SolrExpress.Solr5.Parameter
         public void Execute(JObject jObject)
         {
             var jObj = (JObject)jObject["params"] ?? new JObject();
-           
+
             jObj.Add(new JProperty(this._name, this._value));
 
             jObject["params"] = jObj;
