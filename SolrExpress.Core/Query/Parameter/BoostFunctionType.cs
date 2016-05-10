0b8c42d1eddaae4dd955b256482bb7c2a5663a1b
Diego Luiz Brum <diego.l.brum@gmail.com>
2016-05-10 17:00:21 -0300

    #102
    * Add SolrExpress.Core.Query.Parameter.IBoostParameter<>
    * Add SolrExpress.Core.Query.Parameter.BoostFunctionType

---
+++ SolrExpress.Core/Query/Parameter/BoostFunctionType.cs
@@ -0,0 +1,18 @@
+﻿namespace SolrExpress.Core.Query.Parameter
+{
+    /// <summary>
+    /// Boost's type of the boost calculation
+    /// </summary>
+    public enum BoostFunctionType
+    {
+        /// <summary>
+        /// Sort using "boost" function (multiplicative)
+        /// </summary>
+        Boost,
+
+        /// <summary>
+        /// Sort using "bq" function (additive)
+        /// </summary>
+        Bq
+    }
+}
---
+++ SolrExpress.Core/Query/Parameter/IBoostParameter.cs
@@ -0,0 +1,18 @@
+﻿using SolrExpress.Core.Query.ParameterValue;
+
+namespace SolrExpress.Core.Query.Parameter
+{
+    /// <summary>
+    /// Signatures to use boost parameter
+    /// </summary>
+    public interface IBoostParameter<TDocument> : IParameter
+        where TDocument : IDocument
+    {
+        /// <summary>
+        /// Configure current instance
+        /// </summary>
+        /// <param name="query">Query used to make boost</param>
+        /// <param name="sortType">Boost type used in calculation</param>
+        IBoostParameter<TDocument> Configure(IQueryParameterValue query, FacetSortType? sortType = null);
+    }
+}
