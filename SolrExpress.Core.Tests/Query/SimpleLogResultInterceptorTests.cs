7f9a81898254275ceeef068f1bea56efba4ec6b5
Diego Luiz Brum <diego.l.brum@gmail.com>
2016-05-10 14:19:10 -0300

    #105

---
+++ SolrExpress.Core.Tests/Query/SimpleLogResultInterceptorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolrExpress.Core.Query;
+using System;
+
+namespace SolrExpress.Core.Tests.Query
+{
+    [TestClass]
+    public class SimpleLogResultInterceptorTests
+    {
+        /// <summary>
+        /// Where   Using a SimpleLogResultInterceptor instance
+        /// When    Creating class with null value
+        /// What    Throws ArgumentNullException
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SimpleLogResultInterceptor001()
+        {
+            // Arrange / Act / Assert
+            new SimpleLogResultInterceptor(null);
+        }
+
+        /// <summary>
+        /// Where   Using a SimpleLogResultInterceptor instance
+        /// When    Creating class with empty value
+        /// What    Throws ArgumentNullException
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SimpleLogResultInterceptor002()
+        {
+            // Arrange / Act / Assert
+            new SimpleLogResultInterceptor("");
+        }
+    }
+}
