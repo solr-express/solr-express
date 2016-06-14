a9a232f4bd44b3e220544affa5491dc7fcacaf80
diego <diego@DESKTOP-ELPNNR1>
2016-06-13 23:49:30 -0300

    nuget packages

--- build.bat
+++ build.bat
@@ -2,32 +2,18 @@
 
 REM LOCAL VARIABLES
 SETLOCAL
-SET NUGETAPP=.nuget\nuget.exe
-
-REM CHECK NUGET.EXE, DOWNLOAD IF NECESSARY
-IF EXIST %NUGETAPP% goto updatenuget
-@echo.
-IF NOT EXIST .nuget md .nuget
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile '%NUGETAPP%'"
-goto pack
-
-:updatenuget
-@echo.
-REM %NUGETAPP% update -self
+SET OUTPUT_DIRECTORY=.
 
 :pack
 REM DELETE OLD PACKAGES
+cd .
 del solrexpress.*.nupkg
 
 REM BUILD SIGNED PACKAGES
-.nuget\nuget.exe pack SolrExpress.Core\SolrExpress.Core.csproj -IncludeReferencedProjects -Properties Configuration=Release -Properties DefineConstants=STRONGNAME -Verbosity detailed -build
-.nuget\nuget.exe pack SolrExpress.Solr4\SolrExpress.Solr4.csproj -IncludeReferencedProjects -Properties Configuration=Release -Properties DefineConstants=STRONGNAME -Verbosity detailed -build
-.nuget\nuget.exe pack SolrExpress.Solr5\SolrExpress.Solr5.csproj -IncludeReferencedProjects -Properties Configuration=Release -Properties DefineConstants=STRONGNAME -Verbosity detailed -build
+dotnet pack src/SolrExpress.Core/project.json --configuration release --output .
+dotnet pack src/SolrExpress.Solr4/project.json --configuration release --output .
+dotnet pack src/SolrExpress.Solr5/project.json --configuration release --output .
 
-REM BUILD NOT SIGNED PACKAGES
-.nuget\nuget.exe pack SolrExpress.Core\SolrExpress.Core.csproj -IncludeReferencedProjects -Properties Configuration=Release -Verbosity detailed -build
-.nuget\nuget.exe pack SolrExpress.Solr4\SolrExpress.Solr4.csproj -IncludeReferencedProjects -Properties Configuration=Release -Verbosity detailed -build
-.nuget\nuget.exe pack SolrExpress.Solr5\SolrExpress.Solr5.csproj -IncludeReferencedProjects -Properties Configuration=Release -Verbosity detailed -build
 @echo.
 
 pause
