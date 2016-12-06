ec1665228db412716a798aa0ecef564212e89384
Diego Luiz Brum <diego.l.brum@gmail.com>
2016-12-06 18:50:28 -0200

    Implementation (#167)

--- src/SolrExpress.Core/Utility/ExpressionCache.cs
+++ src/SolrExpress.Core/Utility/ExpressionCache.cs
@@ -1,16 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace SolrExpress.Core.Utility
 {
-    public class ExpressionCache<TDocument> : IExpressionCache<TDocument>
-        where TDocument : IDocument
+    public class ExpressionCache : IExpressionCache
     {
-        void IExpressionCache<TDocument>.Process()
+        private readonly Regex _expressionKeyRegex = new Regex(@"(\w+)\s\=\>\s(Convert\()?\1\.(\w+)\)?", RegexOptions.Compiled);
+        private readonly Dictionary<string, Tuple<PropertyInfo, SolrFieldAttribute>> _internalCache = new Dictionary<string, Tuple<PropertyInfo, SolrFieldAttribute>>();
+
+        /// <summary>
+        /// Get key from informed expression
+        /// </summary>
+        /// <param name="expression">Expression used to get key</param>
+        /// <returns>A key from informed expression</returns>
+        private string GetKeyFromExpression<TDocument>(Expression<Func<TDocument, object>> expression)
         {
-            throw new NotImplementedException();
+            return this._expressionKeyRegex.Match(expression.ToString()).Groups[3].Value;
+        }
+
+        bool IExpressionCache.Get<TDocument>(Expression<Func<TDocument, object>> expression, out PropertyInfo propertyInfo, out SolrFieldAttribute solrFieldAttribute)
+        {
+            propertyInfo = null;
+            solrFieldAttribute = null;
+
+            var key = GetKeyFromExpression(expression);
+
+            if (this._internalCache.ContainsKey(key))
+            {
+                var cache = this._internalCache[key];
+
+                propertyInfo = cache.Item1;
+                solrFieldAttribute = cache.Item2;
+            }
+
+            return false;
+        }
+
+        void IExpressionCache.Set<TDocument>(Expression<Func<TDocument, object>> expression, PropertyInfo propertyInfo, SolrFieldAttribute solrFieldAttribute)
+        {
+            var key = GetKeyFromExpression(expression);
+            var cache = new Tuple<PropertyInfo, SolrFieldAttribute>(propertyInfo, solrFieldAttribute);
+
+            if (this._internalCache.ContainsKey(key))
+            {
+                this._internalCache[key] = cache;
+            }
+            else
+            {
+                this._internalCache.Add(key, cache);
+            }
         }
     }
 }
---
+++ src/SolrExpress.Core/Utility/ExpressionCacheWarmup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace SolrExpress.Core.Utility
+{
+    /// <summary>
+    /// Helper class used in cache warmup
+    /// </summary>
+    internal class ExpressionCacheWarmup<TDocument>
+        where TDocument : IDocument
+    {
+        private ExpressionUtility _expressionUtility;
+
+        internal ExpressionCacheWarmup(ExpressionUtility expressionUtility)
+        {
+            this._expressionUtility = expressionUtility;
+        }
+
+        internal void Load()
+        {
+            var documentParameter = Expression.Parameter(typeof(TDocument), "document");
+            var properties = typeof(TDocument).GetProperties();
+
+            foreach (var propertye in properties)
+            {
+                var nameProperty = Expression.Convert(Expression.Property(documentParameter, propertye.Name), typeof(object));
+
+                var expression = Expression.Lambda<Func<TDocument, object>>(nameProperty, documentParameter);
+
+                this._expressionUtility.GetFieldNameFromExpression(expression);
+                this._expressionUtility.GetSolrFieldAttributeFromPropertyInfo(expression);
+            }
+        }
+    }
+}
