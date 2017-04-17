45014c29d321f56c1a7709b52594aee7a60dc6b6
diegobrum <diego.l.brum@gmail.com>
2017-04-17 10:39:43 -0300

    #187 New example

---
+++ example/SimpleUse.NetCore/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using SolrExpress;
+using SolrExpress.DI.CoreClr;
+using SolrExpress.Search.Parameter.Extension;
+using SolrExpress.Search.Result;
+using SolrExpress.Search.Result.Extension;
+using SolrExpress.Solr5.Extension;
+using System;
+using System.Collections.Generic;
+
+namespace SimpleUse.NetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var services =
+                new ServiceCollection()
+                .AddSolrExpress<TechProduct>(q => q
+                .UseHostAddress("http://localhost:8983/solr/techproducts")
+                .UseSolr5());
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var documentCollection = serviceProvider.GetRequiredService<DocumentCollection<TechProduct>>();
+
+            IEnumerable<TechProduct> documents;
+            IEnumerable<FacetKeyValue> facets;
+            Information information;
+
+            documentCollection
+                .Select()
+                .Fields(d => d.Id, d => d.Manufacturer)
+                .FacetField(d => d.Categories)
+                .Filter(d => d.Id, "205325092")
+                .Execute()
+                .Information(out information)
+                .Document(out documents)
+                .Facets(out facets)
+                .Execute();
+
+            foreach (var document in documents)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(document));
+
+                Console.WriteLine(new string('-', 50));
+            }
+        }
+    }
+}
---
+++ example/SimpleUse.NetCore/TechProduct.cs
@@ -0,0 +1,41 @@
+﻿using SolrExpress;
+using System;
+
+namespace SimpleUse.NetCore
+{
+    public class TechProduct : IDocument
+    {
+        [SolrField("id")]
+        public string Id { get; set; }
+
+        [SolrField("name")]
+        public string Name { get; set; }
+
+        [SolrField("manu")]
+        public string Manufacturer { get; set; }
+
+        [SolrField("manu_id_s")]
+        public string ManufacturerId { get; set; }
+
+        [SolrField("cat")]
+        public string[] Categories { get; set; }
+
+        [SolrField("features")]
+        public string[] Features { get; set; }
+
+        [SolrField("price")]
+        public decimal Price { get; set; }
+
+        [SolrField("popularity")]
+        public decimal Popularity { get; set; }
+
+        [SolrField("inStock")]
+        public bool InStock { get; set; }
+
+        [SolrField("manufacturedate_dt")]
+        public DateTime ManufacturedateIn { get; set; }
+
+        [SolrField("store")]
+        public GeoCoordinate StoredAt { get; set; }
+    }
+}
