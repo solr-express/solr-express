585cc4cbba5ce3b4af36f71979469f6e34fdcfe9
diegobrum <diego.l.brum@gmail.com>
2017-06-19 16:08:27 -0300

    #187 unit tests

---
+++ test/SolrExpress.Solr5.UnitTests/Search/Parameter/QueryFieldParameterTests.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using SolrExpress.Search;
+using SolrExpress.Search.Parameter;
+using SolrExpress.Search.Parameter.Extension;
+using SolrExpress.Solr5.Search.Parameter;
+using Xunit;
+
+namespace SolrExpress.Solr5.UnitTests.Search.Parameter
+{
+    public class QueryFieldParameterTests
+    {
+        /// <summary>
+        /// Where   Using a QueryFieldParameter instance
+        /// When    Invoking the method "Execute"
+        /// What    Create a valid JSON
+        /// </summary>
+        [Fact]
+        public void QueryFieldParameter001()
+        {
+            // Arrange
+            var expected = JObject.Parse(@"
+            {
+              params:{
+                qf:""id^10 score~2^20""
+              }
+            }");
+            var container = new JObject();
+            var parameter = (IQueryFieldParameter<TestDocument>)new QueryFieldParameter<TestDocument>();
+            parameter.Expression("id^10 score~2^20");
+
+            // Act
+            ((ISearchItemExecution<JObject>)parameter).Execute();
+            ((ISearchItemExecution<JObject>)parameter).AddResultInContainer(container);
+
+            // Assert
+            Assert.Equal(expected.ToString(), container.ToString());
+        }
+    }
+}
---
+++ test/SolrExpress.Solr5.UnitTests/Search/Parameter/QueryParameterTests.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using SolrExpress.Builder;
+using SolrExpress.Search;
+using SolrExpress.Search.Parameter;
+using SolrExpress.Search.Query;
+using SolrExpress.Search.Query.Extension;
+using SolrExpress.Solr5.Search.Parameter;
+using Xunit;
+
+namespace SolrExpress.Solr5.UnitTests.Search.Parameter
+{
+    public class QueryParameterTests
+    {
+        /// <summary>
+        /// Where   Using a QueryParameter instance
+        /// When    Invoking the method "Execute"
+        /// What    Create a valid JSON
+        /// </summary>
+        [Fact]
+        public void QueryParameter001()
+        {
+            // Arrange
+            var expected = JObject.Parse("{\"query\": \"_id_:\\\"ITEM01\\\"\"}");
+            var container = new JObject();
+            var parameter = (IQueryParameter<TestDocument>)new QueryParameter<TestDocument>();
+            var solrExpressOptions = new SolrExpressOptions();
+            var expressionBuilder = new ExpressionBuilder<TestDocument>(solrExpressOptions);
+            expressionBuilder.LoadDocument();
+            var searchQuery = new SearchQuery<TestDocument>(expressionBuilder);
+            parameter.Value = searchQuery.Field(q => q.Id).EqualsTo("ITEM01");
+
+            // Act
+            ((ISearchItemExecution<JObject>)parameter).Execute();
+            ((ISearchItemExecution<JObject>)parameter).AddResultInContainer(container);
+
+            // Assert
+            Assert.Equal(expected.ToString(), container.ToString());
+        }
+    }
+}
