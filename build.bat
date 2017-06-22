80576f41fd300059a42cb7c084b7bb02f71b25e4
Arjan Speiard | Rovecom <aspeiard@rovecom.nl>
2017-06-22 12:07:59 +0200

    Added dotnet restore before packing

--- build.bat
+++ build.bat
@@ -10,6 +10,7 @@ cd .
 del solrexpress.*.nupkg
 
 REM BUILD SIGNED PACKAGES
+dotnet restore
 dotnet pack src/SolrExpress.Core/SolrExpress.Core.csproj --configuration release --output .
 dotnet pack src/SolrExpress.Solr4/SolrExpress.Solr4.csproj --configuration release --output .
 dotnet pack src/SolrExpress.Solr5/SolrExpress.Solr5.csproj --configuration release --output .
