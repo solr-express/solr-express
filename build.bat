52763c0af03e59d471f72ec2670ad9b12c934810
Arjan Speiard | Rovecom <aspeiard@rovecom.nl>
2017-06-22 12:03:56 +0200

    Updated build file

--- build.bat
+++ build.bat
@@ -10,9 +10,9 @@ cd .
 del solrexpress.*.nupkg
 
 REM BUILD SIGNED PACKAGES
-dotnet pack src/SolrExpress.Core/project.json --configuration release --output .
-dotnet pack src/SolrExpress.Solr4/project.json --configuration release --output .
-dotnet pack src/SolrExpress.Solr5/project.json --configuration release --output .
+dotnet pack src/SolrExpress.Core/SolrExpress.Core.csproj --configuration release --output .
+dotnet pack src/SolrExpress.Solr4/SolrExpress.Solr4.csproj --configuration release --output .
+dotnet pack src/SolrExpress.Solr5/SolrExpress.Solr5.csproj --configuration release --output .
 
 @echo.
 
