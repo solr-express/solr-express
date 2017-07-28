79352ab61d1938ae4d39ab8a21aaa6bd300dca15
diegobrum <diego.l.brum@gmail.com>
2017-07-28 17:59:26 -0300

    #176 extensions

---
+++ src/SolrExpress/Search/Parameter/Extension/IStandardQueryParameterExtension.cs
@@ -0,0 +1,23 @@
+﻿using SolrExpress.Search.Query;
+
+namespace SolrExpress.Search.Parameter.Extension
+{
+    /// <summary>
+    /// Extensions to configure standard query parameter
+    /// </summary>
+    public static class IStandardQueryParameterExtension
+    {
+        /// <summary>
+        /// Configure parameter to include in standard query
+        /// </summary>
+		/// <param name="parameter">Parameter to congigure</param>
+        /// <param name="value">Parameter to include in standard query</param>
+        public static IStandardQueryParameter<TDocument> Value<TDocument>(this IStandardQueryParameter<TDocument> parameter, SearchQuery value)
+            where TDocument : Document
+        {
+            parameter.Value = value;
+
+            return parameter;
+        }
+    }
+}
---
+++ src/SolrExpress/Search/Parameter/Extension/IWriteTypeParameterExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace SolrExpress.Search.Parameter.Extension
+{
+    /// <summary>
+    /// Extensions to configure write type parameter
+    /// </summary>
+    public static class IWriteTypeParameterExtension
+    {
+        /// <summary>
+        /// Configure write type used in SOLR's result
+        /// </summary>
+		/// <param name="parameter">Parameter to congigure</param>
+        /// <param name="value">Write type used in SOLR's result</param>
+        public static IWriteTypeParameter<TDocument> Value<TDocument>(this IWriteTypeParameter<TDocument> parameter, WriteType value)
+            where TDocument : Document
+        {
+            parameter.Value = value;
+
+            return parameter;
+        }
+    }
+}
