7f1d2255a7ee63229964bebb27a4b4ae79b86dff
diegobrum <diego.l.brum@gmail.com>
2017-08-01 14:44:28 -0300

    #163

--- build.bat
+++ build.bat
@@ -7,13 +7,15 @@ ECHO RESTORE PACKAGES
 dotnet restore
 
 ECHO RUN TESTS
-dotnet test test/SolrExpress.UnitTests --verbosity minimal --framework "netcoreapp1.1" --configuration release
-dotnet test test/SolrExpress.Solr4.UnitTests --verbosity minimal --framework "netcoreapp1.1" --configuration release
-dotnet test test/SolrExpress.Solr5.UnitTests --verbosity minimal --framework "netcoreapp1.1" --configuration release
+dotnet test ./test/SolrExpress.UnitTests/SolrExpress.UnitTests.csproj --verbosity minimal --framework "netcoreapp1.1" --configuration release
+dotnet test ./test/SolrExpress.Solr4.UnitTests/SolrExpress.Solr4.UnitTests.csproj --verbosity minimal --framework "netcoreapp1.1" --configuration release
+dotnet test ./test/SolrExpress.Solr5.UnitTests/SolrExpress.Solr5.UnitTests.csproj --verbosity minimal --framework "netcoreapp1.1" --configuration release
 
 ECHO BUILD PACKAGES
-dotnet pack src/SolrExpress --configuration release --output ..\..
+dotnet pack src/SolrExpress/SolrExpress.csproj --configuration release --output ..\..
 dotnet pack src/SolrExpress.Solr4/SolrExpress.Solr4.csproj --configuration release --output ..\..
 dotnet pack src/SolrExpress.Solr5/SolrExpress.Solr5.csproj --configuration release --output ..\..
 
 pause
+
+
