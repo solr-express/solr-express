59a64f705b9c29e303c8f69ae2c6331b0340cfb5
Diego Luiz Brum <diego.l.brum@outlook.com>
2017-08-29 17:39:57 -0300

    #230 Adde netcoreapp2.0

--- build.bat
+++ build.bat
@@ -7,9 +7,9 @@ ECHO RESTORE PACKAGES
 dotnet restore
 
 ECHO RUN TESTS
-dotnet test ./test/SolrExpress.UnitTests/SolrExpress.UnitTests.csproj --verbosity minimal --framework "netcoreapp1.1" --configuration release
-dotnet test ./test/SolrExpress.Solr4.UnitTests/SolrExpress.Solr4.UnitTests.csproj --verbosity minimal --framework "netcoreapp1.1" --configuration release
-dotnet test ./test/SolrExpress.Solr5.UnitTests/SolrExpress.Solr5.UnitTests.csproj --verbosity minimal --framework "netcoreapp1.1" --configuration release
+dotnet test ./test/SolrExpress.UnitTests/SolrExpress.UnitTests.csproj --verbosity minimal --framework "netcoreapp2.0" --configuration release
+dotnet test ./test/SolrExpress.Solr4.UnitTests/SolrExpress.Solr4.UnitTests.csproj --verbosity minimal --framework "netcoreapp2.0" --configuration release
+dotnet test ./test/SolrExpress.Solr5.UnitTests/SolrExpress.Solr5.UnitTests.csproj --verbosity minimal --framework "netcoreapp2.0" --configuration release
 
 ECHO BUILD PACKAGES
 dotnet pack src/SolrExpress/SolrExpress.csproj --configuration release --output ..\..
