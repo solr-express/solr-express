6627f938f3373f3f8f4fd3ebd0eb9d5f1b55bb5e
Diego Luiz Brum <diego.l.brum@outlook.com>
2019-01-23 19:19:15 +0000

    #380

--- src/SolrExpress/Search/Parameter/Extension/IFacetSpatialParameterExtension.cs
+++ src/SolrExpress/Search/Parameter/Extension/IFacetSpatialParameterExtension.cs
@@ -66,11 +66,11 @@ namespace SolrExpress.Search.Parameter.Extension
         /// Configure distance from center point
         /// </summary>
 		/// <param name="parameter">Parameter to configure</param>
-        /// <param name="distance">Distance from center point</param>
-        public static IFacetSpatialParameter<TDocument> Distance<TDocument>(this IFacetSpatialParameter<TDocument> parameter, decimal distance)
+        /// <param name="value">Distance from center point</param>
+        public static IFacetSpatialParameter<TDocument> Distance<TDocument>(this IFacetSpatialParameter<TDocument> parameter, decimal value)
             where TDocument : Document
         {
-            parameter.Distance = distance;
+            parameter.Distance = value;
 
             return parameter;
         }
