bbb120a63f139cc3073d04e1ad5435b485cc9092
Diego Luiz Brum <diego.l.brum@outlook.com>
2019-01-23 19:25:14 +0000

    #372

--- src/SolrExpress/Search/Parameter/Extension/ICursorMarkParameterExtension.cs
+++ src/SolrExpress/Search/Parameter/Extension/ICursorMarkParameterExtension.cs
@@ -9,10 +9,10 @@
         /// Configure mark used to paging through the results
         /// </summary>
         /// <param name="parameter">Parameter to configure</param>
-        /// <param name="cursorMark">Mark used to paging through the results</param>
-        public static ICursorMarkParameter CursorMark(this ICursorMarkParameter parameter, string cursorMark)
+        /// <param name="value">Mark used to paging through the results</param>
+        public static ICursorMarkParameter CursorMark(this ICursorMarkParameter parameter, string value)
         {
-            parameter.CursorMark = cursorMark;
+            parameter.CursorMark = value;
 
             return parameter;
         }
