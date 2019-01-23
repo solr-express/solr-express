8ec9edf0d2ea2e912da15943dccd11130f8bd03e
Diego Luiz Brum <diego.l.brum@outlook.com>
2019-01-23 19:25:57 +0000

    #373

--- src/SolrExpress/Search/Parameter/Extension/IFacetFieldParameterExtension.cs
+++ src/SolrExpress/Search/Parameter/Extension/IFacetFieldParameterExtension.cs
@@ -26,11 +26,11 @@ namespace SolrExpress.Search.Parameter.Extension
         /// Configure sort type of result of facet
         /// </summary>
 		/// <param name="parameter">Parameter to configure</param>
-        /// <param name="sortType">Sort type of result of facet</param>
-        public static IFacetFieldParameter<TDocument> SortType<TDocument>(this IFacetFieldParameter<TDocument> parameter, FacetSortType sortType)
+        /// <param name="value">Sort type of result of facet</param>
+        public static IFacetFieldParameter<TDocument> SortType<TDocument>(this IFacetFieldParameter<TDocument> parameter, FacetSortType value)
             where TDocument : Document
         {
-            parameter.SortType = sortType;
+            parameter.SortType = value;
 
             return parameter;
         }
