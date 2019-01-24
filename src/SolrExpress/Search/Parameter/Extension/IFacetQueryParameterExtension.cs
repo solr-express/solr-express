7c0f58d232d80df04b4983ea18d49139af985630
Diego Luiz Brum <diegobrum.aubay@gmail.com>
2019-01-24 17:28:05 +0000

    #397

--- src/SolrExpress/Search/Parameter/Extension/IFacetQueryParameterExtension.cs
+++ src/SolrExpress/Search/Parameter/Extension/IFacetQueryParameterExtension.cs
@@ -78,11 +78,11 @@ namespace SolrExpress.Search.Parameter.Extension
         /// Configure limit of itens in facet's result
         /// </summary>
         /// <param name="parameter">Parameter to configure</param>
-        /// <param name="limit">Limit of itens in facet's result</param>
-        public static IFacetQueryParameter<TDocument> Limit<TDocument>(this IFacetQueryParameter<TDocument> parameter, int limit)
+        /// <param name="value">Limit of itens in facet's result</param>
+        public static IFacetQueryParameter<TDocument> Limit<TDocument>(this IFacetQueryParameter<TDocument> parameter, int value)
             where TDocument : Document
         {
-            parameter.Limit = limit;
+            parameter.Limit = value;
 
             return parameter;
         }
