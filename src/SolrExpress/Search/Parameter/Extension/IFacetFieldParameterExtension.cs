9cde713618f86f8c1cd50aa3920a9492ca3aa604
Diego Luiz Brum <diegobrum.aubay@gmail.com>
2019-01-24 17:29:28 +0000

    #393

--- src/SolrExpress/Search/Parameter/Extension/IFacetFieldParameterExtension.cs
+++ src/SolrExpress/Search/Parameter/Extension/IFacetFieldParameterExtension.cs
@@ -65,11 +65,11 @@ namespace SolrExpress.Search.Parameter.Extension
         /// Configure list of tags to exclude in facet calculation
         /// </summary>
 		/// <param name="parameter">Parameter to configure</param>
-        /// <param name="excludes">List of tags to exclude in facet calculation</param>
-        public static IFacetFieldParameter<TDocument> Excludes<TDocument>(this IFacetFieldParameter<TDocument> parameter, params string[] excludes)
+        /// <param name="values">List of tags to exclude in facet calculation</param>
+        public static IFacetFieldParameter<TDocument> Excludes<TDocument>(this IFacetFieldParameter<TDocument> parameter, params string[] values)
             where TDocument : Document
         {
-            parameter.Excludes = excludes;
+            parameter.Excludes = values;
 
             return parameter;
         }
