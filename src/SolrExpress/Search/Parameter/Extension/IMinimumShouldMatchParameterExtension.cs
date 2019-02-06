508ca3d87af66515a415d18afd748a2fc1ec9c60
diegobrum <diego.l.brum@gmail.com>
2019-02-06 14:52:01 +0000

    #383

--- src/SolrExpress/Search/Parameter/Extension/IMinimumShouldMatchParameterExtension.cs
+++ src/SolrExpress/Search/Parameter/Extension/IMinimumShouldMatchParameterExtension.cs
@@ -9,11 +9,11 @@
         /// Configure value used to make mm parameter
         /// </summary>
 		/// <param name="parameter">Parameter to configure</param>
-        /// <param name="value">Value used to make mm parameter</param>
-        public static IMinimumShouldMatchParameter<TDocument> Value<TDocument>(this IMinimumShouldMatchParameter<TDocument> parameter, string value)
+        /// <param name="str">Value used to make mm parameter</param>
+        public static IMinimumShouldMatchParameter<TDocument> Value<TDocument>(this IMinimumShouldMatchParameter<TDocument> parameter, string str)
             where TDocument : Document
         {
-            parameter.Value = value;
+            parameter.Value = str;
 
             return parameter;
         }
